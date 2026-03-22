761ac6b52d67394ea76c642351b7d360d9530d73
luyi25561 <3264812538@qq.com>
2026-03-22 15:43:48 +0800

    version3

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -1,6 +1,8 @@
-
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
-result = num1 + num2
-print(f"Sum of {num1} and {num2} is {result}")
-
+# Version 3
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    result = num1 + num2
+    print(f"Sum of {num1} and {num2} is {result}")
+except ValueError:
+    print("Please enter valid integers!")
