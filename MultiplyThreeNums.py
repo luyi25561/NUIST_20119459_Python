4bc87ed2587b7094d5310ac33c43b6b53f98721c
luyi <3264812538@qq.com>
2026-03-23 08:44:52 +0800

    Add MultiplyThreeNums.py

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,5 @@
+num1= float(input("please enterthe first number"))
+num2=float(input("please enter the second number"))
+num3 = float(input("please enter the third number"))
+product = num1*num2*num3
+print(f"the result is:{product}")
